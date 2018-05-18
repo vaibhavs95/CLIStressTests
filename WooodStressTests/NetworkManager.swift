6287e2b8e8ff608aac099b8c52fc761cee3125c9
vaibhavs95 <vaibhavsingh911@gmail.com>
2018-05-18 13:52:33 +0530

    Added the parameters and the method for creating urlRequests

--- WooodStressTests/NetworkManager.swift
+++ WooodStressTests/NetworkManager.swift
@@ -13,12 +13,20 @@ enum Router {
     case getTimeline
     case getUserFeed(id: Int)
     case createPost(post: Post)
-    case postDetail(id: Int)
+    case getPostDetail(id: Int)
     case likePost(post: Post)
     case comment(post: Post)
     case deletePost(id: Int)
-    case getPostComments(postId: Int)
-    case getPostLikes(postId: Int)
+    case getAllComments(postId: Int)
+    case getAllLikes(postId: Int)
+
+    var method: HTTPMethod {
+        switch self {
+            case .getTimeline, .getUserFeed, .getAllLikes, .getAllComments, .getPostDetail: return .get
+            case .likePost, .comment, .createPost: return .post
+            case .deletePost: return .delete
+        }
+    }
 
     var path: String {
         switch self {
@@ -32,19 +40,54 @@ enum Router {
             return "/api/v1/post"
         case .deletePost(let id):
             return "/api/v1/post/\(id)"
-        case .getUserFeed:
-            return "/api/v1/timeline/user_feed"
-        case .postDetail(let id):
+        case .getUserFeed(let id):
+            return "/api/v1/timeline/user_feed?user_feed=\(id)"
+        case .getPostDetail(let id):
             return "/api/v1/post/\(id)"
-        case .getPostComments(let postId):
+        case .getAllComments(let postId):
             return "/api/v1/post/\(postId)/comments"
-        case .getPostLikes(let postId):
+        case .getAllLikes(let postId):
             return "/api/v1/post/\(postId)/likes"
+        }
+    }
+
+    var parameters: [String: Any]? {
+        switch self {
+            case .getUserFeed(let id):
+                return ["user_id": id]
+            default:
+                return nil
+        }
+    }
+
+    func asURLRequest() throws -> URLRequest? {
+        guard let url = URL(string: NetworkManager.baseUrl)?.appendingPathComponent(path) else
+            { throw APIError.invalidURL(reason: "Url couldn't be generated.") }
+        do {
+            var urlRequest = URLRequest(url: url)
+            urlRequest.httpMethod = method.rawValue
+            urlRequest.allHTTPHeaderFields = NetworkManager.authorizationHeaders
+            let encoder = JSONEncoder()
+            switch self {
+            case .createPost(let post), .likePost(let post), .comment(let post):
+                urlRequest.httpBody = try encoder.encode(post)
+
+                return urlRequest
+            default:
+                return urlRequest
+            }
+        } catch let error {
+            throw APIError.parameterEncodingFailed(reason: error.localizedDescription)
         }
     }
 }
 
-class Manager {
+class NetworkManager {
+
+    static let baseUrl: String = "https://ply-reporter-dev.herokuapp.com"
+    static let authorizationHeaders: [String : String] = ["Authorization" : "Basic YWRtaW46ZG90c2xhc2g=",
+                                                         "Content-Type" : "application/json; charset=utf-8",
+                                                         "User-Language" : "en" ]
 
     func decodeResponse<T: Codable>(data: Data?, type: T.Type, decoder: JSONDecoder = JSONDecoder()) throws -> T? {
         do {
